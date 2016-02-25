ea9eea2f7c065ebdaf9ea7c602a4462354ab38bd
taku <mirror.shot@gmail.com>
2016-02-26 00:30:51 +0900

    first commit

---
+++ sample_form.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ftpRemote
+{
+    partial class sample_form
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadfile = new System.Windows.Forms.Button();
+            this.ServerDirList = new System.Windows.Forms.ListBox();
+            this.btnFileStream = new System.Windows.Forms.Button();
+            this.txtCsvDetail = new System.Windows.Forms.TextBox();
+            this.txtServerDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReadfile
+            // 
+            this.btnReadfile.Location = new System.Drawing.Point(12, 43);
+            this.btnReadfile.Name = "btnReadfile";
+            this.btnReadfile.Size = new System.Drawing.Size(120, 30);
+            this.btnReadfile.TabIndex = 0;
+            this.btnReadfile.Text = "サーバ読み込み";
+            this.btnReadfile.UseVisualStyleBackColor = true;
+            this.btnReadfile.Click += new System.EventHandler(this.btnReadfile_Click);
+            // 
+            // ServerDirList
+            // 
+            this.ServerDirList.FormattingEnabled = true;
+            this.ServerDirList.ItemHeight = 12;
+            this.ServerDirList.Location = new System.Drawing.Point(13, 79);
+            this.ServerDirList.Name = "ServerDirList";
+            this.ServerDirList.Size = new System.Drawing.Size(649, 88);
+            this.ServerDirList.TabIndex = 2;
+            // 
+            // btnFileStream
+            // 
+            this.btnFileStream.Location = new System.Drawing.Point(12, 173);
+            this.btnFileStream.Name = "btnFileStream";
+            this.btnFileStream.Size = new System.Drawing.Size(120, 30);
+            this.btnFileStream.TabIndex = 0;
+            this.btnFileStream.Text = "ファイルストリーミング";
+            this.btnFileStream.UseVisualStyleBackColor = true;
+            this.btnFileStream.Click += new System.EventHandler(this.btnFileStream_Click);
+            // 
+            // txtCsvDetail
+            // 
+            this.txtCsvDetail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCsvDetail.Location = new System.Drawing.Point(13, 210);
+            this.txtCsvDetail.Multiline = true;
+            this.txtCsvDetail.Name = "txtCsvDetail";
+            this.txtCsvDetail.Size = new System.Drawing.Size(648, 157);
+            this.txtCsvDetail.TabIndex = 3;
+            // 
+            // txtServerDirectory
+            // 
+            this.txtServerDirectory.Location = new System.Drawing.Point(75, 12);
+            this.txtServerDirectory.Name = "txtServerDirectory";
+            this.txtServerDirectory.Size = new System.Drawing.Size(200, 19);
+            this.txtServerDirectory.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "サーバURL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ユーザー";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(353, 12);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(90, 19);
+            this.txtUser.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(457, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "パスワード";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(515, 12);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(90, 19);
+            this.txtPass.TabIndex = 8;
+            // 
+            // sample_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 379);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServerDirectory);
+            this.Controls.Add(this.txtCsvDetail);
+            this.Controls.Add(this.ServerDirList);
+            this.Controls.Add(this.btnFileStream);
+            this.Controls.Add(this.btnReadfile);
+            this.Name = "sample_form";
+            this.Text = "サンプルアプリ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadfile;
+        private System.Windows.Forms.ListBox ServerDirList;
+        private System.Windows.Forms.Button btnFileStream;
+        private System.Windows.Forms.TextBox txtCsvDetail;
+        private System.Windows.Forms.TextBox txtServerDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
+
---
+++ sample_form.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+
+// namespace 
+namespace ftpRemote
+{
+    /// <summary>
+    /// サンプルプログラムクラス
+    /// </summary>
+    public partial class sample_form : Form
+    {
+        /// <summary>
+        /// サーバファイル情報の正規表現
+        /// </summary>
+        private string regex =
+        @"^" +                          //# Start of line
+        @"(?<dir>[\-ld])" +             //# File size          
+        @"(?<permission>[\-rwx]{9})" +  //# Whitespace          \n
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<filecode>\d+)" +
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<owner>\w+)" +
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<group>\w+)" +
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<size>\d+)" +
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<month>\w{3})" +            //# Month (3 letters)   \n
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<day>\d{1,2})" +            //# Day (1 or 2 digits) \n
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<timeyear>[\d:]{4,5})" +    //# Time or year        \n
+        @"\s+" +                        //# Whitespace          \n
+        @"(?<filename>(.*))" +          //# Filename            \n
+        @"$";                           //# End of line
+
+        // 接続ID/PASS
+        private NetworkCredential m_cred = null;
+
+        /// <summary>
+        /// サーバのディレクトリパス
+        /// </summary>
+        private const string DEFAULT_DIRECTORY = "ftp://localhost/sftp-user/";
+        
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public sample_form()
+        {
+            InitializeComponent();
+
+            txtServerDirectory.Text = DEFAULT_DIRECTORY;
+        }
+        
+        /// <summary>
+        /// サーバのファイル読み込み処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReadfile_Click(object sender, EventArgs e)
+        {
+            //リストの初期化
+            ServerDirList.Items.Clear();
+            try {
+                // 接続ID/PASS
+                NetworkCredential credent = getNetworkCredential();
+                string directory = string.Empty;
+
+                if (credent != null)
+                {
+                    directory = txtServerDirectory.Text;
+                    GetServerDirData(directory, credent);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// ネットワーク認証情報の作成
+        /// </summary>
+        /// <returns>ネットワーク認証</returns>
+        private NetworkCredential getNetworkCredential()
+        {
+            //ユーザ名.パスワードない場合は処理を抜ける
+            if (string.IsNullOrEmpty(txtUser.Text) || string.IsNullOrEmpty(txtPass.Text)) {
+                MessageBox.Show(this,"ユーザ名もしくはパスワードが存在しません。","認証情報未入力",MessageBoxButtons.OK,MessageBoxIcon.Information );
+                return null; 
+            }
+            // 認証情報を返す
+            return new NetworkCredential(txtUser.Text, txtPass.Text);
+        }
+        
+        /// <summary>
+        /// サーバの指定FTPディレクトリの取得
+        /// </summary>
+        /// <param name="serverDir">サーバディレクトリ</param>
+        /// <param name="credent">認証情報</param>
+        private bool GetServerDirData(string serverDir,NetworkCredential credent)
+        {
+            bool dirDataGetResult = false ;
+            //ファイル一覧を取得するディレクトリのURI
+            Uri u = new Uri(serverDir);
+
+            FtpWebRequest ftp = null;
+
+            try
+            {
+                // FTPWebRequestの取得
+                ftp = (FtpWebRequest)GetFtpWebRequest(credent, u, WebRequestMethods.Ftp.ListDirectoryDetails);
+
+                //FtpWebResponseを取得
+                using (FtpWebResponse ftpRes =
+                    (FtpWebResponse)ftp.GetResponse())
+                {
+                    //FTPサーバーから送信されたデータを取得
+                    using (System.IO.StreamReader sr =
+                        new System.IO.StreamReader(ftpRes.GetResponseStream()))
+                    {
+                        while (true)
+                        {
+                            string line = sr.ReadLine();
+                            if (line == null) break;
+
+                            ServerDirList.Items.Add(line);
+                        }
+
+                        sr.Close();
+                    }
+                    //閉じる
+                    ftpRes.Close();
+
+                    dirDataGetResult = true;
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+            }
+            return dirDataGetResult;
+        }
+        /// <summary>
+        /// ファイルストリーム取得
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFileStream_Click(object sender, EventArgs e)
+        {
+            // 1件のときのみ取得可能
+            if (ServerDirList.SelectedItems.Count == 1) {
+                // 接続ID/PASS
+                NetworkCredential credent = getNetworkCredential();
+                string directory = string.Empty;
+
+                if (credent != null)
+                {
+                    directory = txtServerDirectory.Text;
+                    DataStreaming(directory, GetFileName((string)ServerDirList.SelectedItem), credent);
+                }
+
+            } else {
+                MessageBox.Show("選択は1件のみ可能となっております。未選択、複数選択では処理できません。");
+            }
+        }
+
+
+        /// <summary>
+        /// サーバのファイル情報からファイル名を取得する
+        /// </summary>
+        /// <param name="serverList"></param>
+        /// <returns></returns>
+        private string GetFileName(string serverList)
+        {
+            var split = new Regex(regex).Match(serverList);
+            string dir = split.Groups["dir"].ToString();
+            string filename = split.Groups["filename"].ToString();
+            bool isDirectory = !string.IsNullOrWhiteSpace(dir) && dir.Equals("d", StringComparison.OrdinalIgnoreCase);
+
+            return filename;
+        }
+
+        /// <summary>
+        /// ストリーミング処理の実施
+        /// </summary>
+        /// <param name="serverDir">サーバディレクトリ</param>
+        /// <param name="filename">ファイル名</param>
+        /// <param name="credent">認証情報</param>
+        private void DataStreaming(string serverDir, string filename, NetworkCredential credent)
+        {
+            //ファイル一覧を取得するディレクトリのURI
+            Uri u = new Uri(serverDir + filename);
+            FtpWebRequest ftpReq = null;
+            try
+            {
+                txtCsvDetail.Text = "";
+
+                // FTPWebRequestの取得
+                ftpReq = (FtpWebRequest)GetFtpWebRequest(credent, u, WebRequestMethods.Ftp.DownloadFile);
+
+                using (WebResponse res = ftpReq.GetResponse())
+                using (StreamReader st = new StreamReader(res.GetResponseStream(),
+                    System.Text.Encoding.GetEncoding("shift_jis")))
+                {
+                    string results = st.ReadToEnd();
+                    st.Close();
+
+                    txtCsvDetail.Text = results;
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+            }
+
+        }
+
+        /// <summary>
+        /// FTPRequestを構築
+        /// </summary>
+        /// <param name="credent"></param>
+        /// <returns></returns>
+        private FtpWebRequest GetFtpWebRequest(
+            NetworkCredential credent,
+            Uri accesspath,
+            string methodType)
+        {
+            //FtpWebRequestの作成
+            FtpWebRequest ftpReq = null;
+
+            try
+            {
+                //FtpWebRequestの作成
+                ftpReq = (FtpWebRequest)
+                    WebRequest.Create(accesspath);
+
+                //ログインユーザー名とパスワードを設定
+                ftpReq.Credentials = credent;
+
+                //処理タイプの設定
+                ftpReq.Method = methodType;
+                //要求の完了後に接続を閉じる
+                ftpReq.KeepAlive = false;
+                //PASSIVEモードを無効にする
+                ftpReq.UsePassive = false;
+                //Binaryモードを無効
+                ftpReq.UseBinary = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "エラー");
+            }
+
+            return ftpReq;
+        }
+
+    }
+}
